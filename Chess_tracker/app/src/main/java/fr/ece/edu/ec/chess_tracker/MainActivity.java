ea98b22c64b03dad6ecef9da3ce8be9fa66ec900
Cerveau Eric <eric.cerveau@hotmail.fr>
2020-11-13 20:53:30 +0100

    Adding ressources + creating Playing layout

--- Chess_tracker/app/src/main/java/fr/ece/edu/ec/chess_tracker/MainActivity.java
+++ Chess_tracker/app/src/main/java/fr/ece/edu/ec/chess_tracker/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.github.bhlangonijr.chesslib.Board;
+import com.github.bhlangonijr.chesslib.Square;
+import com.github.bhlangonijr.chesslib.move.Move;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
---
+++ Chess_tracker/app/src/main/java/fr/ece/edu/ec/chess_tracker/RegisterGame.java
@@ -0,0 +1,14 @@
+package fr.ece.edu.ec.chess_tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_game);
+    }
+}
